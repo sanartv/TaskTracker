36030d11984c14f603e7c6c64205e4b51cb58154
sanartv <sannikov.artem.vl@gmail.com>
2026-02-25 13:04:08 +0500

    Created DockerFile

--- src/test/java/com/Polazna/TaskTracker/TaskTrackerApplicationTests.java
+++ src/test/java/com/Polazna/TaskTracker/TaskTrackerApplicationTests.java
@@ -2,8 +2,10 @@ package com.Polazna.TaskTracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TaskTrackerApplicationTests {
 
 	@Test
